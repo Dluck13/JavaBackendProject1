de11cd4500a98ebc1b8f4b1aaea243979b7a9f24
Dluck13 <dimitri.luck@revature.net>
2022-03-09 15:36:31 -0500

    employee main complete, needs to be tested

--- src/main/java/Service/Project1Main.java
+++ src/main/java/Service/Project1Main.java
@@ -1,8 +1,11 @@
 package Service;
 
+import java.util.List;
+
 import POJO.Employee;
 import POJO.Reimbursement;
 import io.javalin.Javalin;
+
 
 public class Project1Main {
 
@@ -45,6 +48,52 @@ public class Project1Main {
 		myServer.get("/Login", (ctx)->{
 			Employee e = ctx.bodyAsClass(Employee.class);
 			ctx.json(LoginService.login(e));
+		});
+		
+		
+		
+		EmployeeService employeeService = new EmployeeServiceImpl();
+		
+		
+		// all employees
+		myServer.get("/employeess", (ctx)->{
+			List<Employee> allEmployees = employeeService.fetchAllEmployees();
+			ctx.json(allEmployees);
+		});
+		
+		//fetch employee
+		myServer.get("/employeess/{employee_id}", (ctx)->{
+			//retrieve the path param value,specify path param key
+			String empId = ctx.pathParam("empid");
+			//tell service layer to fetch book
+			Employee fetchedEmployee = employeeService.fetchAEmployee(Integer.parseInt(empId));
+			//return book
+			ctx.json(fetchedEmployee);
+		});
+		//delete an employee
+		myServer.delete("/employeess/{employee_id}", (ctx)->{
+			//retrieve the path param value,specify path param key
+			String empId = ctx.pathParam("empid");
+			//tell service layer to delete
+			Employee deletedEmp = employeeService.deleteEmployee(Integer.parseInt(empId));
+			//return book
+			ctx.json(deletedEmp);});
+		
+		//add a employee
+		myServer.post("/employeess", (ctx)->{
+			//converts json to pojo
+		Employee newEmp = ctx.bodyAsClass(Employee.class);
+		Employee returnedEmp = employeeService.addEmployee(newEmp);
+		ctx.json(returnedEmp);
+		});
+		
+		//update a 
+		
+		myServer.put("/emps", (ctx)->{
+			
+		Employee newUpdate = ctx.bodyAsClass(Employee.class);
+		Employee returnedUpdate = employeeService.updateEmployee(newUpdate);
+		ctx.json(returnedUpdate);
 		});
 		
 		
